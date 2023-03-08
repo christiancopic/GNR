ce6fa0b366647b6e3f293a76cedf2280ca1d89f4
christiancopic <silvercopic@gmail.com>
2023-03-07 17:06:04 -0700

    add electrical characteristics func

--- data_parser.py
+++ data_parser.py
@@ -13,27 +13,41 @@ def main():
     for file in os.listdir(cwd + '/devicedataset'):
         if count < 2:
             if file[-4:] == '.csv':
-                make_graph(file)
+                data = pd.read_csv(cwd + '/devicedataset/' + file)
+                device_name = file.split('[')[1].split(']')[0]
+                make_graph(data, device_name)
+                e_char(data, device_name)
                 count = count + 1
         else:
             continue
 
-def make_graph(file):
-    cwd = os.getcwd()
-    data = pd.read_csv(cwd + '/devicedataset/' + file)
-    plt.semilogy(figsize=(6.8, 4.2))
+# Create ID vs. VGS curves of data
+def make_graph(data, device_name):
+    #log y with regular x axis
+    plt.semilogy(figsize=(9, 5))
     data['ID'] = data['ID'] * -1
     VD_values = data['VD'].unique()
-    #print(data)
     for i in range(len(VD_values)):
-        subdf = data[data["VD"] == VD_values[i]]
-        #print(subdf)
+        subdf = data[data['VD'] == VD_values[i]]
         plt.plot(subdf['VG'], subdf['ID'], label = '$V_{DS}$ = ' + str(VD_values[i]) + 'V')
+    
     # Use LaTeX for labels
     plt.xlabel('$V_{GS}$ (V)')
     plt.ylabel('$-I_{D}$ (A)')
+    plt.title('$-I_{D}$ (A) vs. $V_{GS}$ (V) from ' + device_name)
     plt.legend()
     plt.show()
+
+# Calculate electrical characteristics of device
+def e_char(data, device_name):
+    VD_values = data['VD'].unique()
+    for i in range(len(VD_values)):
+        subdf = data[data['VD'] == VD_values[i]]
+        print('For VD = ' + str(VD_values[i]) + ' on device ' + device_name + ': ')
+        print('Min Value: ' + str(subdf['ID'].min(0)))
+        print('Max Value: ' + str(subdf['ID'].max(0)))
+        print('Ion/Ioff ratio: ' + str(subdf['ID'].max(0) / subdf['ID'].min(0)))
+        
 
 if __name__ == '__main__':
     main()
