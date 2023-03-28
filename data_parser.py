7e97d23f2d0b73ceafd408180ff1c21ab1653ace
christiancopic <silvercopic@gmail.com>
2023-03-27 18:27:28 -0700

    pptx creation (WIP) & gitignore for csv files

--- data_parser.py
+++ data_parser.py
@@ -7,9 +7,12 @@ import pandas as pd
 from pptx import Presentation
 from pptx.util import Inches
 import math
+import csv
+import time
 
 
 def main():
+    start = time.time()
     cwd = os.getcwd()
     # Adding count for debugging purposes
     count = 0
@@ -26,6 +29,7 @@ def main():
         else:
             continue
     create_pptx()
+    print('Done. Total time: ' + str(time.time() - start))
 
 # Create ID vs. VGS curves of data
 def make_graph(data, device_name):
@@ -51,6 +55,7 @@ def make_graph(data, device_name):
 
 # Calculate electrical characteristics of device
 def e_char(data, device_name):
+    edata=[] # Figure out better method but this works in mean time
     VD_values = data['VD'].unique()
     for i in range(len(VD_values)):
         subdf = data[data['VD'] == VD_values[i]]
@@ -69,20 +74,28 @@ def e_char(data, device_name):
             high_vg = float(high_df['VG'].to_string(index=False, header=False))
             low_vg = float(low_df['VG'].to_string(index=False, header=False))
             
-            high_id = float(high_df['moving'].to_string(index=False, header=False))
-            low_id = float(low_df['moving'].to_string(index=False, header=False))
+            high_id = high_df.values[0][-1]
+            low_id = low_df.values[0][-1]
 
             delta_vg = high_vg-low_vg
             delta_id = math.log10(high_id) - math.log10(low_id)
             SS = delta_vg/delta_id
-            print('SUBTHRESHOLD for VDS= ' + str(VD_values[i]) + ' : ' + str(SS))
+            #print('SUBTHRESHOLD for VDS= ' + str(VD_values[i]) + ' : ' + str(SS))
+
+            edata.append(SS)
+            edata.append(j['moving'].min(0))
+            edata.append(j['moving'].max(0))
+            edata.append(j['moving'].max(0)/j['moving'].min(0))
             #print(str(j))
             #print('For VD = ' + str(VD_values[i]) + ' on device ' + device_name + ': ')
             #print('Min ID Value: ' + str(j['ID'].min(0)))
             #print('Max ID Value: ' + str(j['ID'].max(0)))
             #print('Ion/Ioff ratio: ' + str(j['ID'].max(0) / subdf['ID'].min(0)))
-            if(VD_values[i] == -1.0): print('Max IG Value: ' + str(j['IG'].max(0)))
+            if(VD_values[i] == -1.0): 
+                #print('Max IG Value: ' + str(j['IG'].max(0)))
+                edata.append(j['IG'].max(0))
 
+    make_csv(edata, device_name)
     #put into csv/xlsx file code here:
     #also add maximum IG value for -1V = VDS
 
@@ -102,6 +115,25 @@ def create_pptx():
             os.remove(file)
  
     ppt.save('data_parser.pptx')
+
+def make_csv(data, device_name):
+    cwd = os.getcwd()
+    if not os.path.exists(cwd + '/excelFiles'):
+        os.makedirs(cwd + '/excelFiles')
+
+    # This is ugly :(
+    rows = [[str(device_name), 'Forward Sweep', '', 'Reverse Sweep', ''], 
+         ['',           '0.1V',             '1V',                '0.1V',            '1V'], 
+         ['Ion',        str(data[6]),       str(data[15]),        str(data[2]),      str(data[10])], 
+         ['Ioff',       str(data[5]),       str(data[14]),        str(data[1]),      str(data[9])], 
+         ['Ion/Ioff',   str(data[7]),       str(data[16]),        str(data[3]),      str(data[11])], 
+         ['SS',         str(data[4]),       str(data[13]),        str(data[0]),      str(data[8])],
+         ['Max Ig',     'n/a',              str(data[17]),        'n/a',             str(data[12])]]
+    #print('DATA: ' + str(data))
+
+    with open(cwd + '/excelFiles/' + device_name + '.csv', 'w') as csvfile: 
+        csvwriter = csv.writer(csvfile) 
+        csvwriter.writerows(rows)
 
 if __name__ == '__main__':
     main()
